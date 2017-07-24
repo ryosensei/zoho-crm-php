f430cc961ce0f2113b62c13cf5ce33d71714bfe5
Marc VARLET <marc@saebaweb.com>
2017-07-24 08:11:46 -0700

    Add Service Provider class

---
+++ src/ZohoServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Zoho\CRM;
+
+use Illuminate\Support\ServiceProvider;
+
+class ZohoServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
